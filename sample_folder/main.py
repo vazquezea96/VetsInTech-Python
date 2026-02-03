f19d00af7746cc4469d68ab2fde30c59c4d46652
Eduardo Vazquez <80133388+vazquezea96@users.noreply.github.com>
2026-02-03 10:32:57 -0800

    Week 1 started. Day 1 complete.

---
+++ sample_folder/main.py
@@ -0,0 +1,45 @@
+print("Hello, world!")
+print(42)
+print("The answer is", 42)
+print('hello')
+print("Python")
+print("That's my favorite book")
+print('He said "What is your name?"')
+
+print("Line 1\nLine 2\nLine 3")
+print("""This is line 1.
+This is line 2.
+This is line 3.""")
+# This is a comment line.
+# Python skips this when executing.
+
+print("This will run.") # This is an inline comment.
+
+my_variable = "a string"
+age = 25
+pi = 3.14
+
+# valid exmples
+my_name = "Jael"
+_name = "Andre"
+name1 = "Student"
+
+# invalid examples:
+# 1name = "error" # starts with a number 
+# my-name = "error" # contains a hypen
+
+# Multi-world variable names
+MyFavoriteBand = "Pierce the Veil" # PascalCase
+myFavoriteFood = "Pizza" # camelCase
+my_favorite_programming_language = "Python" # snake_case preferred in Python
+
+# Printing variables
+name = "Eddie"
+print(name)
+
+# User Input with input()
+""" The input() function lets your program receive information from the user.
+It shows a prompt, waits for the user to type something, and then returns that text as string.  """
+
+user_age = input("What is your age? ")
+print("Wow, you look good for " + user_age)
